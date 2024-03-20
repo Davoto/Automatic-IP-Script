c88f63cbb5ac20e017e794162977bf4e7fcdf9dc
Davoto <tijsknechtgm@gmail.com>
2024-03-20 12:05:40 +0000

    Script finished

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
+import subprocess
+import sys
+import requests
+import datetime
+from dotenv import load_dotenv
 import os
 
 
 def get_ip():
-    host = os.system('hostname -I')
-    return str(host)[:-2]
+    host_ip = subprocess.check_output("hostname -I", shell=True).decode(sys.stdout.encoding).strip()
+    return host_ip
 
 
-def cloudflare_update(ip):
-    test = 0
+def cloudflare_update(host_ip, domain, zone_id, dns_id, auth_key, auth_mail):
+    url = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{dns_id}"
+
+    payload = {
+        "content": host_ip,
+        "name": domain,
+        "proxied": False,
+        "type": "A",
+        "comment": f'IP Update, {datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}',
+        "ttl": 1
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "X-Auth-Email": auth_mail,
+        "Authorization": auth_key,
+    }
+
+    response = requests.request("PATCH", url, json=payload, headers=headers)
+
+    print(response.text)
 
 
 if __name__ == '__main__':
-    print(get_ip())
+    load_dotenv()
+    ip = get_ip()
+    AUTH_KEY = os.getenv('AUTH_KEY')
+    AUTH_MAIL = os.getenv('AUTH_MAIL')
+    DOMAIN = os.getenv('DOMAIN')
+    ZONE_ID = os.getenv('ZONE_ID')
+    DNS_ID = os.getenv('DNS_ID')
+    print(f"Dit is het lokale ip: {ip}")
+    cloudflare_update(ip, DOMAIN, ZONE_ID, DNS_ID, AUTH_KEY, AUTH_MAIL)
