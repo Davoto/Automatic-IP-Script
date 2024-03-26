a472b2784279632aacd3799a71890d1d50c7602c
Davoto <tijsknechtgm@gmail.com>
2024-03-26 14:18:29 +0000

    Made API-Results more readable.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import datetime
+import os
+import requests
 import subprocess
 import sys
-import requests
-import datetime
+from json import loads, dumps
 from dotenv import load_dotenv, set_key
-import os
 
 
 def get_ip():
@@ -28,9 +29,7 @@ def cloudflare_update(host_ip, domain, zone_id, dns_id, auth_key, auth_mail):
         "Authorization": auth_key,
     }
 
-    response = requests.request("PATCH", url, json=payload, headers=headers)
-
-    print(response.text)
+    return loads(requests.request("PATCH", url, json=payload, headers=headers).text)
 
 
 if __name__ == '__main__':
@@ -38,15 +37,23 @@ if __name__ == '__main__':
     load_dotenv(path, override=True)
     LOCAL_IP_NEW = get_ip()
     LOCAL_IP = os.getenv('LOCAL_IP')
-    print(f"This is the local IP: {LOCAL_IP_NEW}")
+    print(f"This is the local IP: \"{LOCAL_IP_NEW}\".")
     if LOCAL_IP_NEW != LOCAL_IP:
         AUTH_KEY = os.getenv('AUTH_KEY')
         AUTH_MAIL = os.getenv('AUTH_MAIL')
         DOMAIN = os.getenv('DOMAIN')
         ZONE_ID = os.getenv('ZONE_ID')
         DNS_ID = os.getenv('DNS_ID')
-        os.environ['LOCAL_IP'] = LOCAL_IP_NEW
-        set_key(path, 'LOCAL_IP', os.environ['LOCAL_IP'])
-        cloudflare_update(LOCAL_IP_NEW, DOMAIN, ZONE_ID, DNS_ID, AUTH_KEY, AUTH_MAIL)
+
+        json_result = cloudflare_update(LOCAL_IP_NEW, DOMAIN, ZONE_ID, DNS_ID, AUTH_KEY, AUTH_MAIL)
+        if json_result['success']:
+            os.environ['LOCAL_IP'] = LOCAL_IP_NEW
+            set_key(path, 'LOCAL_IP', os.environ['LOCAL_IP'])
+
+            print(f'API-CALL Successful.\n{json_result["result"]["name"]} has been updated.')
+        else:
+            print('API-CALL Unsuccessful.\nPlease check your .env file in the program\'s folder.\n'
+                  'Here the JSON for further debugging:')
+            print(dumps(json_result, indent=2))
     else:
         print('IP has not changed.')
